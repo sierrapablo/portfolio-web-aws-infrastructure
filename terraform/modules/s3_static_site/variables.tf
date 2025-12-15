28c0be21da1c960bc5d0c93682937fefb6150804
Pablo Sierra Lorente <pablosierralorente@gmail.com>
2025-12-16 00:36:00 +0100

    remove unused variables

--- terraform/modules/s3_static_site/variables.tf
+++ terraform/modules/s3_static_site/variables.tf
@@ -8,15 +8,3 @@ variable "enable_versioning" {
   type        = bool
   default     = false
 }
-
-variable "cloudfront_oai_iam_arn" {
-  description = "IAM ARN of CloudFront Origin Access Identity"
-  type        = string
-  default     = ""
-}
-
-variable "cloudfront_distribution_arn" {
-  description = "ARN of CloudFront distribution"
-  type        = string
-  default     = ""
-}
